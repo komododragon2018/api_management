fa3142b6fd4ee53ca3557813e862137473949493
User <user@Users-MacBook-Pro-2.local>
2018-06-14 12:00:32 +0100

    more changes

--- packages/backend/db.js
+++ packages/backend/db.js
@@ -1,2 +1,2 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/phonebook', {autoReconnect: true},);
+mongoose.connect('mongodb://mongodb/phonebook', {autoReconnect: true},);

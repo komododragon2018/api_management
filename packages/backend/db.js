23c83a02e9c823952340db18845e6fe12144d54e
User <user@Users-MacBook-Pro-2.local>
2018-06-14 13:04:32 +0100

    Got it finally working

--- packages/backend/db.js
+++ packages/backend/db.js
@@ -1,2 +1,12 @@
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://mongodb/phonebook', {autoReconnect: true},);
+var mongoose = require('mongoose')
+var mongoUrl = "mongodb://mongodb/phonebook"
+
+var connectWithRetry = function() {
+  return mongoose.connect(mongoUrl, function(err) {
+    if (err) {
+      console.error('Failed to connect to mongo on startup - retrying in 5 sec', err);
+      setTimeout(connectWithRetry, 5000);
+    }
+  });
+};
+connectWithRetry();
